af3eff85463eee8e25b2e412528b0b526342abb7
Pazaz <pazaz@itspazaz.com>
2022-04-19 20:40:36 -0400

    Rename Class22_Sub1 to SoftReferenceNodeFactory

--- client/src/main/java/SoftReferenceNodeFactory.java
+++ client/src/main/java/SoftReferenceNodeFactory.java
@@ -3,7 +3,7 @@ import org.openrs2.deob.annotation.OriginalClass;
 import org.openrs2.deob.annotation.OriginalMember;
 
 @OriginalClass("client!db")
-public final class Class22_Sub1 extends Class22 {
+public final class SoftReferenceNodeFactory extends Class22 {
 
 	@OriginalMember(owner = "client!db", name = "a", descriptor = "(Lclient!gf;I)Lclient!gf;")
 	@Override
--- client/src/main/java/Static44.java
+++ client/src/main/java/Static44.java
@@ -57,7 +57,7 @@ public final class Static44 {
 	@OriginalMember(owner = "client!dh", name = "b", descriptor = "(I)Lclient!q;")
 	public static Class22 method1147() {
 		try {
-			return (Class22) Class.forName("Class22_Sub1").getDeclaredConstructor().newInstance();
+			return (Class22) Class.forName("SoftReferenceNodeFactory").getDeclaredConstructor().newInstance();
 		} catch (@Pc(15) Throwable local15) {
 			return null;
 		}
