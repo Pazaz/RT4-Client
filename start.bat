bc7ecce61cc3e55ff8934ffb9f9496877a0e2fff
Pazaz <pazaz@itspazaz.com>
2022-04-20 21:50:21 -0400

    New gradle scripts to modularize the build process

--- start.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-call .\gradlew.bat build
-java -jar .\build\libs\RT4-Client-1.0.0.jar 1 live english game0
--- start.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-./gradlew build
-java -jar ./build/libs/RT4-Client-1.0.0.jar 1 live english game0
