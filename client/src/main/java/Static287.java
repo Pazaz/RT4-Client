76b3c7d3a6c83b6ed9a64cdab94b47622d8f1a5e
Pazaz <pazaz@itspazaz.com>
2022-06-18 23:17:00 -0400

    fix: Commented out JSObject code to support newer JDKs

--- client/src/main/java/Static287.java
+++ client/src/main/java/Static287.java
@@ -1,5 +1,4 @@
 import java.applet.Applet;
-import netscape.javascript.JSObject;
 import org.openrs2.deob.annotation.OriginalArg;
 import org.openrs2.deob.annotation.OriginalMember;
 
@@ -7,16 +6,16 @@ public final class Static287 {
 
 	@OriginalMember(owner = "client!gh", name = "a", descriptor = "(Ljava/lang/String;BLjava/applet/Applet;)Ljava/lang/Object;")
 	public static Object method1757(@OriginalArg(0) String arg0, @OriginalArg(2) Applet arg1) throws Throwable {
-		return JSObject.getWindow(arg1).call(arg0, (Object[]) null);
+		return null; // JSObject.getWindow(arg1).call(arg0, (Object[]) null);
 	}
 
 	@OriginalMember(owner = "client!gh", name = "a", descriptor = "(Ljava/applet/Applet;Ljava/lang/String;[Ljava/lang/Object;B)Ljava/lang/Object;")
 	public static Object method1758(@OriginalArg(0) Applet arg0, @OriginalArg(1) String arg1, @OriginalArg(2) Object[] arg2) throws Throwable {
-		return JSObject.getWindow(arg0).call(arg1, arg2);
+		return null; // JSObject.getWindow(arg0).call(arg1, arg2);
 	}
 
 	@OriginalMember(owner = "client!gh", name = "a", descriptor = "(Ljava/applet/Applet;ZLjava/lang/String;)V")
 	public static void method1759(@OriginalArg(0) Applet arg0, @OriginalArg(2) String arg1) throws Throwable {
-		JSObject.getWindow(arg0).eval(arg1);
+		// JSObject.getWindow(arg0).eval(arg1);
 	}
 }
